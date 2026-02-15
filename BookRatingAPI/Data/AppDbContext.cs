8d93ad33ceb5cb641815a062f16184e5256e36a2
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2026-02-15 22:50:39 +0100

    Creating User DTO

--- BookRatingAPI/Data/AppDbContext.cs
+++ BookRatingAPI/Data/AppDbContext.cs
@@ -1,8 +1,9 @@
+using BookRatingAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookRatingAPI.Data;
 
 public class AppDbContext : DbContext
 {
-    
+    public DbSet<User> Users { get; set; }
 }
