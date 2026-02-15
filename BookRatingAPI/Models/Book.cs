9b3b87af941247c14a791f6b7bae306035810a30
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2026-02-15 22:35:36 +0100

    Create Book model

---
+++ BookRatingAPI/Models/Book.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookRatingAPI.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = string.Empty;
+    
+    [Required]
+    [MaxLength(100)]
+    public string Author { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+    
+    public string? CoverImageUrl { get; set; }
+    
+    public int? PublicationYear { get; set; }
+    
+    public string ISBN { get; set; }
+    
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public ICollection<Rating> Ratings { get; set; } = new List<Rating>();
+    public ICollection<ReadingList> ReadingLists { get; set; } = new List<ReadingList>();
+}
