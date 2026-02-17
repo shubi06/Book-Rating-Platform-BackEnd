44efe91625aba4fc0ac8a82e878f65109650ba89
Jeta <jfazliu07@gmail.com>
2026-02-17 22:10:11 +0100

    Profile

---
+++ BookRatingAPI/Controllers/ProfileController.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using BookRatingAPI.DTOs;
+using BookRatingAPI.Services;
+
+namespace BookRatingAPI.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class ProfileController : ControllerBase
+{
+    private readonly IProfileService _profileService;
+
+    public ProfileController(IProfileService profileService)
+    {
+        _profileService = profileService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ProfileDto>> GetMyProfile()
+    {
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        var profile = await _profileService.GetMyProfileAsync(userId);
+
+        if (profile == null)
+            return NotFound();
+
+        return Ok(profile);
+    }
+
+    [HttpGet("{userId}")]
+    [AllowAnonymous]
+    public async Task<ActionResult<PublicProfileDto>> GetUserProfile(int userId)
+    {
+        var profile = await _profileService.GetUserProfileAsync(userId);
+
+        if (profile == null)
+            return NotFound();
+
+        return Ok(profile);
+    }
+}
--- BookRatingAPI/obj/Release/net8.0/BookRatingAPI.AssemblyInfo.cs
+++ BookRatingAPI/obj/Release/net8.0/BookRatingAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookRatingAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6624f077351a0a483e3e262d634c3aa66a13919a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookRatingAPI")]
