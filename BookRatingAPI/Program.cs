a762e2d025f52a3838a8104c200665a1e6ddd52a
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2026-02-18 21:52:54 +0100

    Added IProfileService into Program.cs

--- BookRatingAPI/Program.cs
+++ BookRatingAPI/Program.cs
@@ -60,6 +60,7 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IReadingListService, ReadingListService>();
+builder.Services.AddScoped<IProfileService, ProfileService>();
 
 builder
     .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
