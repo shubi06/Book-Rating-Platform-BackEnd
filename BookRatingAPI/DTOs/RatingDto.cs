173facb5f7bf2103e7e79288ff27e2b489f991b5
jonpercuku <jonper2005@gmail.com>
2026-02-20 00:50:06 +0100

    Updated book ratings (final)

---
+++ BookRatingAPI/DTOs/RatingDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BookRatingAPI.DTOs
+{
+	public class RatingDto
+	{
+		public int Id { get; set; }
+		public int BookId { get; set; }
+		public int UserId { get; set; }
+		public string Username { get; set; } = string.Empty;
+		public int Score { get; set; }
+		public string? Comment { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
